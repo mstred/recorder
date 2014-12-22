aae55196054e251829cd1767e378afa5755f4ade
Edson Samuel Jr <samuedson@gmail.com>
2014-12-22 16:01:59 -0300

    adding drawer

--- app/src/main/java/br/org/cesar/recorder/MainActivity.java
+++ app/src/main/java/br/org/cesar/recorder/MainActivity.java
@@ -2,6 +2,7 @@ package br.org.cesar.recorder;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new RecorderFragment())
